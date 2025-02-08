2f32b399e9031ce9303c658efada1be9fea79432
vanguard-bit <prajwalll205@gmail.com>
2025-02-08 23:29:16 +0530

    works but not fast

--- DisplayImage.cpp
+++ DisplayImage.cpp
@@ -40,7 +40,7 @@ class parallelConvolution : public ParallelLoopBody {
 
   public:
     parallelConvolution(Mat& src, Mat &dst, Mat kernel, vector<vector<vector<char>>> &a)
-      : m_src(src), m_dst(dst), m_kernel(kernel), ascii_art(a) {
+      : m_src(src), m_dst(dst), m_kernel(kernel), ascii_art(a){
         assert(kernel.rows == kernel.cols && kernel.cols== 3);
         // If using a large kernel create another method that iterates -half of rows to +half of rows
         // cout << m_src;
@@ -121,18 +121,47 @@ class parallelConvolution : public ParallelLoopBody {
 
         // calculate current postion(i, j) and return if not possible to create roi
         int i = r / (m_src.cols * channels), j = r % (m_src.cols * channels);
-        if(i + 8 > rows || j + (8 * channels) > cols * channels)
+        if(i + 4 > rows || j + (4 * channels) > cols * channels)
           continue;
 
         // create roi strating from current postion (i, j)
-        Mat roi(8, 8, CV_8U, Scalar(0));
-        for(int x = 0; x < 8; x++){
+        Mat roi(4, 4, CV_8U, Scalar(0));
+        uchar max1 = 0, max2 = 0, max3 = 0;
+        int max_arr[3][256] = {0, 0, 0};
+        int max_val;
+        for(int x = 0; x < 4; x++){
           uchar *roi_ptr = roi.ptr(x);
-          uchar *src_ptr = m_src.ptr(i + x);
-          for(int y = 0; y < 8; y++){
-            roi_ptr[y] = src_ptr[j + (channels * y)];
+          int *src_ptr = m_src.ptr<int>(i + x);
+          for(int y = 0; y < 4; y++){
+            int val = src_ptr[j + (channels * y)];
+            uchar val1, val2, val3;
+            val3 = val & ((1 << 8) - 1);
+            val2 = (val >> 8) & ((1 << 8) - 1);
+            val1 = (val >> 16) & ((1 << 8) - 1);
+            roi_ptr[y] = saturate_cast<uchar>((val1 + val2  + val3) / 3);
+            val1 /= 8;
+            val2 /= 8;
+            val3 /= 8;
+            max_arr[0][val1] += 1;
+            max_arr[1][val2] += 1;
+            max_arr[2][val3] += 1;
           }
         }
+        int max_ele[3];
+        for(int coli = 0; coli < 3; coli++){
+          int most_reps = 0;
+          int index = 0;
+          for(int colj = 0; colj < 255; colj++){
+            // cout << max_arr[coli][colj] << endl;
+            if(max_arr[coli][colj] > most_reps){
+                most_reps = max_arr[coli][colj];
+                index = colj;
+            }
+          }
+          // cout << index << endl;
+          max_ele[coli] = (index * 8);
+        }
+        max_val = (max_ele[0] << 16) + (max_ele[1] << 8) + max_ele[2];
         // cout << roi << endl;
 
 
@@ -142,11 +171,11 @@ class parallelConvolution : public ParallelLoopBody {
         for(int chari = 0; chari < 95; chari++){
           
           // combining currect 8 x 8 image with char matrix
-          Mat curr_mat(8, 8, CV_64F, Scalar(0.0));
-          for(int x = 0; x < 8; x++){
+          Mat curr_mat(4, 4, CV_64F, Scalar(0.0));
+          for(int x = 0; x < 4; x++){
             uchar *roi_ptr = roi.ptr(x);
             double *curr_ptr = curr_mat.ptr<double>(x);
-            for(int y = 0; y < 8; y++){
+            for(int y = 0; y < 4; y++){
               curr_ptr[y] = saturate_cast<double>(
                   abs(static_cast<int>(char_matrices[chari][x][y] - roi_ptr[y])));
             }
@@ -154,9 +183,9 @@ class parallelConvolution : public ParallelLoopBody {
           // cout << curr_mat << endl;
           // convolute each pixel and sum up the value
           double total_val = 0;
-          for(int convi = 0; convi < 8; convi++){
-            for(int convj = 0; convj < 8; convj++){
-              total_val += convolute_d(curr_mat, 0, 0, 8, 8);
+          for(int convi = 0; convi < 4; convi++){
+            for(int convj = 0; convj < 4; convj++){
+              total_val += convolute_d(curr_mat, 0, 0, 4, 4);
               // cout << convolute_d(curr_mat, 0, 0, 8, 8) << " ";
             }
             // cout << endl;
@@ -188,7 +217,8 @@ class parallelConvolution : public ParallelLoopBody {
           // cout << "working";
           ascii_art[j % channels][i][j] = static_cast<char>(min_index);
         }
-        m_dst.at<uchar>(i, j) = saturate_cast<uchar>(min_val); 
+        m_dst.at<int>(i, j) = saturate_cast<int>(max_val); 
+        // cout << "here maybe" << endl;
       }
     }
     friend ostream& operator<<(ostream& out, parallelConvolution obj){
@@ -197,24 +227,35 @@ class parallelConvolution : public ParallelLoopBody {
     }
 };
 
-int main(int argc, char** argv )
-{
+int main(int argc, char** argv ) {
 
     Mat image, new_image;
-    image = imread("C:\\Users\\Prajwal\\OneDrive\\Desktop\\new\\img.png", IMREAD_COLOR);
+    image = imread("C:\\Users\\Prajwal\\OneDrive\\Desktop\\new\\Cool00086400.jpg", IMREAD_COLOR);
     if ( !image.data )
     {
         printf("No image data \n");
         return -1;
     }
-    cv::resize(image, image, cv::Size(255, 70));
+    cv::resize(image, image, cv::Size(240, 60));
     int top = (int) (10); 
     int bottom = top;
     int left = (int) (10); 
     int right = left;
+    Mat color_image(image.rows, image.cols, CV_32S);
+    for(int i = 0; i < image.rows; i++){
+      for(int j = 0; j < image.cols; j++){
+        uchar val1, val2, val3;
+        int val;
+        val1 = image.at<uchar>(i, (j * image.channels()));
+        val2 = image.at<uchar>(i, (j * image.channels()) + 1);
+        val3 = image.at<uchar>(i, (j * image.channels()) + 2);
+        val = (val1 << 16) + (val2 << 8) + val3;
+        color_image.at<int>(i, j) = val;
+      }
+    }
     copyMakeBorder(image, image, top, bottom, left, right, BORDER_CONSTANT, Scalar::all(255));
-    cvtColor(image, image, COLOR_BGR2GRAY);
-    new_image = Mat(image.size(), image.type(), Scalar::all(0));
+    // cvtColor(image, image, COLOR_BGR2GRAY);
+    new_image = Mat(color_image.size(), color_image.type(), Scalar::all(0));
     Mat mat = (Mat_<uchar>(8, 8) << 
         1, 1, 0, 0, 1, 1, 0, 0, 
         1, 1, 1, 0, 1, 1, 1, 0, 
@@ -245,7 +286,7 @@ int main(int argc, char** argv )
         vector<vector<char>>(image.rows, 
           vector<char>(image.cols, '\0')));
     auto t = getTickCount();
-    parallelConvolution obj(image, new_image, kernel, ascii_art); 
+    parallelConvolution obj(color_image, new_image, kernel, ascii_art); 
     parallel_for_(Range(0, image.rows * (image.cols * image.channels())), obj);
     // parallel_for_(Range((image.rows * (image.cols * image.channels())) / 2, (image.rows * (image.cols * image.channels())) / 2 + 1), obj);
     long seconds = (getTickCount() - t) / getTickFrequency();
@@ -254,45 +295,35 @@ int main(int argc, char** argv )
     int hours = minutes / 60;
     minutes %= 60;
     cout << hours << ":" << minutes << ":" << seconds << endl;
-    imwrite("C:\\Users\\Prajwal\\OneDrive\\Desktop\\new\\image25.png", new_image);
+    imwrite("C:\\Users\\Prajwal\\OneDrive\\Desktop\\new\\image25.png", image);
     ofstream myfile;
     stringstream output;
-    myfile.open("C:\\Users\\Prajwal\\OneDrive\\Desktop\\new\\image25.txt");
+    myfile.open("C:\\Users\\Prajwal\\OneDrive\\Desktop\\new\\image26.txt");
     myfile << "Writing this to a file.\n";
     for(int i = 0; i < new_image.rows; i++){
       for(int j = 0; j < new_image.cols; j++){
         char ele = ascii_art[0][i][j];
-        int val = new_image.at<uchar>(i, j);
-        val /= 11;
-        val += 231;
+        int val = new_image.at<int>(i, j);
+        uchar val1, val2, val3;
+        val3 = val & ((1 << 8) - 1);
+        val2 = (val >> 8) & ((1 << 8) - 1);
+        val1 = (val >> 16) & ((1 << 8) - 1);
         if(ele == ' ' || ele == '\0'){
-          output << format("\033[38;5;%dmM\033[0m", val);
-          myfile << format("\033[38;5;%dmM\033[0m", val);
+          output << format("\033[38;2;%d;%d;%dmM\033[0m", val3, val2, val1);
+          myfile << format("\033[38;2;%d;%d;%dmM\033[0m", val3, val2, val1);
         }
         else{
-          myfile << format("\033[38;5;%dm%c\033[0m", val, ele);
-          output << format("\033[38;5;%dm%c\033[0m", val, ele);
+          myfile << format("\033[38;2;%d;%d;%dm%c\033[0m", val3, val2, val1, ele);
+          output << format("\033[38;2;%d;%d;%dm%c\033[0m", val3, val2, val1, ele);
         }
       }
       myfile << '\n';
       output << "\n";
-
       } 
-    // for(auto row: ascii_art[0]){
-    //   for(auto ele: row){
-    //     if(ele == ' ' || ele == '\0'){
-    //       myfile << 'M';
-    //     }
-    //     else{
-    //       myfile << ele;
-    //     }
-    //   }
-    //   myfile << '\n';
-    // }
     myfile.close();
-    // namedWindow("Display Image", WINDOW_KEEPRATIO);
-    // imshow("Display Image", new_image);
-    // waitKey(0);
+    // // namedWindow("Display Image", WINDOW_KEEPRATIO);
+    // // imshow("Display Image", new_image);
+    // // waitKey(0);
     cout << output.str() << endl;
 
     return 0;
